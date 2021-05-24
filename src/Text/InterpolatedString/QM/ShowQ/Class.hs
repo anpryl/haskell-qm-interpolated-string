98c9013550eb60206a5353a3166624169d636888
Anatolii Prylutskyi <anpryl@gmail.com>
2021-05-24 20:10:49 +0300

    Decrease ShowQ instances amount

--- src/Text/InterpolatedString/QM/ShowQ/Class.hs
+++ src/Text/InterpolatedString/QM/ShowQ/Class.hs
@@ -25,18 +25,3 @@ instance ShowQ Char where
 
 instance ShowQ String where
   showQ = id
-
-instance ShowQ Strict.ByteString where
-  showQ = Strict.unpack
-
-instance ShowQ Lazy.ByteString where
-  showQ = Lazy.unpack
-
-instance ShowQ T.Text where
-  showQ = T.unpack
-
-instance ShowQ LazyT.Text where
-  showQ = LazyT.unpack
-
-instance Show a => ShowQ a where
-  showQ = show
